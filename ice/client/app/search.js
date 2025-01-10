ab3a796870d43406fe574fe5b0e3197af59964f8
RajveerMashruwala <156444559+RajveerMashruwala@users.noreply.github.com>
2025-01-10 14:26:49 -0500

    this file contain index.html file

---
+++ ice/client/app/search.js
@@ -0,0 +1,7 @@
+/*
+    Name: Adam Kunz
+    filename: app.js
+    Course: INFT 2202
+    Date: 10/01/2025
+    Description: This is my general search script.  Functions that are required on every page should live here.
+*/
