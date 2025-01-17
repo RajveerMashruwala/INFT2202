bc0f69bebeebcb0b9d64cb581491048a9c979041
RajveerMashruwala <156444559+RajveerMashruwala@users.noreply.github.com>
2025-01-17 14:14:36 -0500

    update

--- ice/client/app/app.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name: Adam Kunz
-    filename: app.js
-    Course: INFT 2202
-    Date: 10/01/2025
-    Description: This is my general application script.  Functions that are required on every page should live here.
-*/
