fdae89ec602527a2b43859435d435dc25486b146
RajveerMashruwala <156444559+RajveerMashruwala@users.noreply.github.com>
2025-01-31 14:17:34 -0500

    test1

---
+++ test1/src/client/data/movies.js
@@ -0,0 +1,141 @@
+export const movies = [{
+  "title": "Ginger Snaps Back: The Beginning",
+  "genre": "Horror",
+  "release_date": "675949371",
+  "release_timestamp": "1991-06-11T06:49:31Z",
+  "director": "Essa MacAindreis",
+  "rating": 2.9
+}, {
+  "title": "Vlad",
+  "genre": "Comedy",
+  "release_date": "1482858623",
+  "release_timestamp": "2016-12-27T19:30:23Z",
+  "director": "Willow Scimoni",
+  "rating": 4.0
+}, {
+  "title": "White Material",
+  "genre": "Action",
+  "release_date": "1165439402",
+  "release_timestamp": "2006-12-07T22:30:02Z",
+  "director": "Evy Groarty",
+  "rating": 6.3
+}, {
+  "title": "Earth to Echo",
+  "genre": "Horror",
+  "release_date": "867481215",
+  "release_timestamp": "1997-06-22T04:06:55Z",
+  "director": "Susanne Ewart",
+  "rating": 9.7
+}, {
+  "title": "White Man's Burden",
+  "genre": "Sci-Fi",
+  "release_date": "1573824411",
+  "release_timestamp": "2019-11-15T17:06:51Z",
+  "director": "Carolan Northfield",
+  "rating": 7.3
+}, {
+  "title": "First a Girl",
+  "genre": "Comedy",
+  "release_date": "1556113850",
+  "release_timestamp": "2019-04-24T23:44:10Z",
+  "director": "Brandais Rennocks",
+  "rating": 6.6
+}, {
+  "title": "Men Seeking Women",
+  "genre": "Horror",
+  "release_date": "659091906",
+  "release_timestamp": "1990-11-06T10:31:46Z",
+  "director": "Hestia Errigo",
+  "rating": 5.4
+}, {
+  "title": "Late Great Planet Earth, The",
+  "genre": "Horror",
+  "release_date": "1431043802",
+  "release_timestamp": "2015-05-07T02:50:02Z",
+  "director": "Corney Shernock",
+  "rating": 8.4
+}, {
+  "title": "Sunrise: A Song of Two Humans",
+  "genre": "Drama",
+  "release_date": "807463701",
+  "release_timestamp": "1995-08-31T22:35:01Z",
+  "director": "Catriona Emmet",
+  "rating": 5.5
+}, {
+  "title": "Leaving Normal",
+  "genre": "Sci-Fi",
+  "release_date": "1058350467",
+  "release_timestamp": "2003-11-14T02:14:27Z",
+  "director": "Laurie Huc",
+  "rating": 1.9
+}, {
+  "title": "All American Orgy (Cummings Farm)",
+  "genre": "Drama",
+  "release_date": "1037896048",
+  "release_timestamp": "2002-11-21T18:27:28Z",
+  "director": "Mirelle Orbine",
+  "rating": 7.3
+}, {
+  "title": "Karate Kid, The",
+  "genre": "Drama",
+  "release_date": "836742331",
+  "release_timestamp": "1996-06-27T23:38:51Z",
+  "director": "Harald Towner",
+  "rating": 6.6
+}, {
+  "title": "Battle of the Rails, The (La bataille du rail)",
+  "genre": "Drama",
+  "release_date": "1357414692",
+  "release_timestamp": "2013-01-05T08:38:12Z",
+  "director": "Vittoria Ofield",
+  "rating": 9.7
+}, {
+  "title": "Double Suicide (Shinjû: Ten no amijima)",
+  "genre": "Drama",
+  "release_date": "1510221191",
+  "release_timestamp": "2017-11-09T02:59:51Z",
+  "director": "Evered Brickhill",
+  "rating": 4.8
+}, {
+  "title": "Assassination (Ansatsu) (Assassination, The) (Assassin, The)",
+  "genre": "Comedy",
+  "release_date": "1171292849",
+  "release_timestamp": "2007-02-12T00:54:09Z",
+  "director": "Wesley Doogan",
+  "rating": 9.5
+}, {
+  "title": "Harrison Bergeron",
+  "genre": "Comedy",
+  "release_date": "870523677",
+  "release_timestamp": "1997-08-07T06:54:37Z",
+  "director": "Marylin Estoile",
+  "rating": 4.5
+}, {
+  "title": "Drive Angry",
+  "genre": "Sci-Fi",
+  "release_date": "668762630",
+  "release_timestamp": "1991-03-04T18:10:30Z",
+  "director": "Jess Solway",
+  "rating": 7.7
+}, {
+  "title": "Tale of Two Cities, A",
+  "genre": "Drama",
+  "release_date": "697363737",
+  "release_timestamp": "1992-02-13T06:55:37Z",
+  "director": "Charissa Seawell",
+  "rating": 2.9
+}, {
+  "title": "Killing Us Softly 3",
+  "genre": "Action",
+  "release_date": "1154357825",
+  "release_timestamp": "2006-07-31T19:37:05Z",
+  "director": "Kevon Heliot",
+  "rating": 1.4
+}, {
+  "title": "Casting Couch",
+  "genre": "Comedy",
+  "release_date": "1417554041",
+  "release_timestamp": "2014-12-02T22:00:41Z",
+  "director": "Janey Smallcombe",
+  "rating": 2.4
+}];
